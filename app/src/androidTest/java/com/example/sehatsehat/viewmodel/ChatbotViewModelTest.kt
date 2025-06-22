07455a377e79cfdb35022a3619cd7d43241e299c
Ferdinand Susanto Halim <119311557+NubStupid@users.noreply.github.com>
2025-06-22 21:57:05 +0700

    added more testing to repository

---
+++ app/src/androidTest/java/com/example/sehatsehat/viewmodel/ChatbotViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.example.sehatsehat.viewmodel
+//
+//import kotlinx.coroutines.ExperimentalCoroutinesApi
+//import org.junit.Assert.*
+//import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+//import org.junit.Before
+//import org.junit.Rule
+//import org.junit.Test
+//
+//@OptIn(ExperimentalCoroutinesApi::class)
+//class ChatbotViewModelTest {
+//
+//    @get:Rule
+////    val rule = InstantTaskExecutorRule()
+//    @Before
+//    fun setUp() {
+//    }
+//
+//    @Test
+//    fun init() {
+//    }
+//}
